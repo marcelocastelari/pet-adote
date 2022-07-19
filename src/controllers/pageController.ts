c79b8f3c9c4c9cc163b1b4698c6b4874d5d8f31d
marcelocastelari <marcelocastelari@gmail.com>
2022-07-19 07:44:42 -0300

    configurado o search e 404 page

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -30,21 +30,25 @@ export const dogs = (req: Request, res: Response) => {
 }
 
 export const cats = (req: Request, res: Response) => {
+    let list = Pet.getFromType('cat');
     res.render('pages/page', {
         menu: createMenuObject('cat'),
         banner: {
             title: 'Gatos',
             background: 'banner_cat.jpg'
-        }
+        },
+        list
     });
 }
 
 export const fishes = (req: Request, res: Response) => {
+    let list = Pet.getFromType('fish');
     res.render('pages/page', {
         menu: createMenuObject('fish'),
         banner: {
             title: 'Peixes',
             background: 'banner_fish.jpg'
-        }
+        },
+        list
     });
 }
